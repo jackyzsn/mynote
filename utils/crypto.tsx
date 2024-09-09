7494d9ae8e7afea90bca38729d41a6865da75f32
Jacky Zhang <jacky.zsn@gmail.com>
2024-09-08 20:20:05 -0400

    update

--- utils/crypto.tsx
+++ utils/crypto.tsx
@@ -1,10 +1,10 @@
 import CryptoJS from 'react-native-crypto-js';
 
-export function encrypt(text, key) {
+export function encrypt(text: string, key: string) {
   return CryptoJS.AES.encrypt(text, key).toString();
 }
 
-export function decrypt(ciphertext, key) {
+export function decrypt(ciphertext: string, key: string) {
   let bytes = CryptoJS.AES.decrypt(ciphertext, key);
 
   return bytes.toString(CryptoJS.enc.Utf8);
